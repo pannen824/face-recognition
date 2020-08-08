858ff5992a15ae483bfc43da20fda93738988bae
Niranjan <pannen824@gmail.com>
2020-08-08 08:46:17 -0500

    Finished design of face-recognition app

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import Tilt from 'react-tilt';
 import './Logo.css';
-
+import welcome from './welcome-image.jpg';
 const Logo = () => {
     return (
-        <div className = 'Logo'>
-            
+        <div>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 250, width: 250 }} >
+                <div className="Tilt-inner [a"> <img style = {{paddingTop: '5px'}} alt = 'logo' src = { welcome }></img></div>
+            </Tilt>
         </div>
     );
 }
 
-export default Logo;
+export default Logo; 
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Rank = () => {
+    return(
+        <div>
+            <div className = 'light-red f3'>
+                {'Niranjan, your current rank is...'}
+            </div>
+            <div className = 'light-red f1'>
+                {'#1'}
+            </div>
+        </div>
+    )
+}
+
+export default Rank;
